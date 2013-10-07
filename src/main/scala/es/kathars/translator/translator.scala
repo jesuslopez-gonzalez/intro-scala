52541ec7835259b924e2fa4444c45a41c6fcffa3
Jesús López-González <jesus.lopez@hablapps.com>
2013-10-07 19:40:13 +0200

    Added all use cases to translator.

--- src/main/scala/es/kathars/translator/translator.scala
+++ src/main/scala/es/kathars/translator/translator.scala
@@ -7,18 +7,7 @@ trait Translator {
   def translate(word: String): Option[String] = dictionary.get(word)
 }
 
-object Translator {
-
-  def printWord(word: String)(implicit t: Option[Translator] = None) = {
-    val w = (t flatMap { _.translate(word) }).getOrElse(word)
-    println(s"\n###> $w\n")
-  }
-
-  implicit def optTranslator(implicit t: Translator): Option[Translator] =
-    Option(t)
-}
-
-object TranslatorImpls {
+object translators {
 
   implicit val english = new Translator {
     val dictionary = Map("hola" -> "hello")
@@ -29,9 +18,34 @@ object TranslatorImpls {
   }
 }
 
+object Translator {
+
+  object myPrint {
+    def apply(word: String)(implicit t: Option[Translator] = None) = {
+      val w = (t flatMap { _.translate(word) }).getOrElse(word)
+      println(s"\n###> $w\n")
+    }
+  }
+
+  implicit def optTranslator(implicit t: Translator): Option[Translator] =
+    Option(t)
+}
+
 object TranslateApp extends App {
   import Translator._ 
-  // import TranslatorImpls.italian
 
-  printWord("hola")
+  myPrint("hola")
+  myPrint("adios")
+
+  {
+    import translators.italian
+    myPrint("hola")
+    myPrint("adios")
+  }
+
+  {
+    import translators.english
+    myPrint("hola")
+    myPrint("adios")
+  }
 }
